74fff253fd39719b3e68c063cf0e969dde5fe646
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2024-06-25 05:18:22 +0100

    done with search function in products page

--- app/products/page.js
+++ app/products/page.js
@@ -1,18 +1,47 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import ProductsList from '../../components/products'
 import { Products } from '../products'
 
 function ProuctList() {
-  return (
-    <div className='flex flex-row'>
-      <div className='w-1/6 h-full '></div>
-      <div className='w-5/6 h-full flex gap-5 flex-wrap '>
-      {Products.map((product) => (
 
-        <ProductsList  imgUrl={product.imgUrl} product={product.title} comparedPrice={product.comparedPrice} price={product.price} reviews={product.reviews}/>
+  const [searchValue, setSearchValue] = useState('')
+  const [filteredItems, setFilteredItems] = useState(Products)
+
+
+  useEffect(() => {
+    setFilteredItems(
+
+      Products.filter((product) => product.title.toLowerCase().includes(searchValue.toLowerCase())) 
+      
+    )
+  }, [searchValue])
+
+
+  return (
+    <div className=' '>
+      <div className='w-full h-full mt-10 flex justify-center'>
+        <input type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} placeholder='Place your Search' className='bg-gray-100 rounded-l-lg focus:outline-none p-2 w-[400px]'/>
+        <button className='text-white bg-black p-2 rounded-r-lg' onClick={() => setFilteredItems(() => {
+          Products.filter((product) => product.title.toLowerCase().includes(searchValue.toLowerCase()))
+        })}>Search</button>
+      </div>
+
+      <div className='flex flex-row '>
+
+
+
+      <div className=' flex w-full justify-center gap-5 flex-wrap  mt-20 '>
+      
+      {filteredItems.map((product, index) => (
+        <div key={index}>
+
+        <ProductsList  imgUrl={product.imgUrl} product={product.title} comparedPrice={product.comparedPrice} price={product.price} reviews={product.reviews} id={product.id}/>
+        </div>
       ))}
       </div>
-    </div>
+      </div>
+      </div>
   )
 }
 
--- components/products.js
+++ components/products.js
@@ -1,4 +1,6 @@
+"use client"
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 import { IoEyeOutline } from 'react-icons/io5'
 import { RiStarSFill } from 'react-icons/ri'
@@ -6,13 +8,18 @@ import { RiStarSFill } from 'react-icons/ri'
 
 
 function ProductsList(props) {
+  const router = useRouter()
   return (
     <div className='mb-20'>
 
         <div className='h-80 w-80 flex  flex-col gap-2'>
                             <div className='absolute ml-[280px] p-1 mt-5 hover:text-white hover:bg-red-500 bg-white z-20  rounded-full  '>
         
-                            <IoEyeOutline className=' text-2xl '/>
+                            <IoEyeOutline className=' text-2xl '                    
+                        onClick={() => {
+                          router.push(`../product/${props.id}`)            
+                         
+                        }}/>
                             </div>
                                     <div className='w-80 h-[215px]  overflow-hidden top-0 rounded z-0 relative group' >
                                       <Image src={`${props.imgUrl[0]}`}  alt='dsfa' width={800} height={300} className='z-10' />      
